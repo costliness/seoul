2810cd55f58516077da37e5d415ede122e996752
joy <85165417+costliness@users.noreply.github.com>
2021-06-03 16:57:39 +0100

    Update welc.js

--- welc.js
+++ welc.js
@@ -1,34 +1,17 @@
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require('discord.js');
 
-module.exports={
-	name: 'help',
-	description: 'shows all the commands of the bot',
+module.exports = {
+	name: 'welc',
+	description: 'shows the number of the members in the server',
 	async execute(message, args, client){
-                    const prefix = client.prefix
-		if(args[0]) {
-			const ctg = client.commands.get(args[0])
-			if(!ctg) return message.reply('Invalid Command')
-			const name = ctg.name
-			const desc = ctg.description
-			
-			const ctgEmbed = new MessageEmbed()
-			 .setTitle(`Name: ${name} \nDescription: ${desc}`)
-                               .setColor('BLACK')
-			 .setTimestamp();
-			 
-			 message.channel.send(ctgEmbed)
-		} else {
-			let cmds = client.commands.map(command => command.name)
-			
-			const allEmbed = new MessageEmbed()
-			.setTitle('hi welcome to seoul!')
-			.setDescription('my prefix is **s;**\n\n``av/avatar`` - shows the mentioned or your avatar\n``members`` - shows you how many members are in the specified server\n`uptime` - shows the uptime of the bot\n`weather (county)` - shows you the weather in a specified country\n``welc`` - show a welcome message!\n\nmore soon lol!!')
-			.setThumbnail(client.user.displayAvatarURL())
-                              .setColor('FFFAFA')
-                              .setFooter("")
-			
-			 message.channel.send(allEmbed)
-			
-		}
+		const { guild } = message
+		const memberCount = guild.memberCount
+		
+		const memberCountEmbed = new MessageEmbed()
+		 .setTitle('welcome to seoul!')
+		 .setDescription('<:w_line:836330544346169345> talk to our members <#832692299715706887>\n<:w_line:836330544346169345> get your roles here! <#832692299715706883>\n<:w_line:836330544346169345> get some colours/colors <#840368261798952991>! \n<:w_line:836330544346169345> check out <#842882810162708491> and support!\n<:w_line:836330544346169345> make sure to read <#842216342790733844>!!\n\nlastly, welcome to seoul! <a:w_hearts:836330544224272384>\n\n[**・our invite link ・**](https://discord.gg/milfs)')
+		 .setColor('FFFAFA');
+		 
+		 message.channel.send(memberCountEmbed)
 	}
 }
